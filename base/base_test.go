b30381b696a4a75a055ff615b894a6a9746d47cd
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2023-12-27 15:57:52 +0800

    slice

--- base/base_test.go
+++ base/base_test.go
@@ -36,3 +36,26 @@ func Test1(t *testing.T) {
 		p.Show()
 	}
 }
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func Test2(t *testing.T) {
+	var arr []*User
+	// 以上创建方式不能直接赋值  arr[0] = ..  可以使用append 会自动扩容
+	arr = append(arr, &User{
+		Name: "zhangsan",
+		Age:  18,
+	})
+	fmt.Printf("%+v\n", arr[0])
+
+	var arr2 = make([]User, 2, 2)
+	// 以上创建方式同样也不能直接赋值  arr[0] = ..  make创建的第一个参数为长度  第二个参数为容量 指定长度内可以直接赋值  容量可以使用append的时候不会频繁扩容
+	arr2[1] = User{
+		Name: "lisi",
+		Age:  18,
+	}
+	fmt.Printf("%+v\n", arr2)
+}
