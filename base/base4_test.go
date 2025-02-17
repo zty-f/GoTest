ed7b44f13b63941352069b7b45fccb3c4890452e
zty-f <zruler@163.com>
2025-02-17 20:43:26 +0800

    defer

--- base/base4_test.go
+++ base/base4_test.go
@@ -46,3 +46,42 @@ func TestDeferPanic(t *testing.T) {
 
 	fmt.Println("main 正常结束")
 }
+
+func DeferFunc1(i int) (t int) {
+	t = i
+	defer func() {
+		t += 3
+	}()
+	return t
+}
+
+func DeferFunc2(i int) int {
+	t := i
+	defer func() {
+		t += 3
+	}()
+	return t
+}
+
+func DeferFunc3(i int) (t int) {
+	defer func() {
+		t += i
+	}()
+	return 2
+}
+
+func DeferFunc4() (t int) {
+	defer func(i int) {
+		fmt.Println(i)
+		fmt.Println(t)
+	}(t)
+	t = 1
+	return 2
+}
+
+func TestDeferSum(t *testing.T) {
+	fmt.Println(DeferFunc1(1))
+	fmt.Println(DeferFunc2(1))
+	fmt.Println(DeferFunc3(1))
+	DeferFunc4()
+}
