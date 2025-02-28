84f46c2e43c08a293f6afab736a8bf0089d9fd3d
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2025-02-28 16:54:54 +0800

    双指针

--- algorithm/01algorithm_test.go
+++ algorithm/01algorithm_test.go
@@ -119,13 +119,6 @@ func minIncrements(n int, cost []int) int {
 	return res
 }
 
-func abs(x int) int {
-	if x > 0 {
-		return x
-	}
-	return -x
-}
-
 // 4. [27]移除元素
 /*
 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
--- algorithm/04_algorithm_double_point_slide_window_test.go
+++ algorithm/04_algorithm_double_point_slide_window_test.go
@@ -215,6 +215,76 @@ func threeSum(nums []int) [][]int {
 	return res
 }
 
+// 4、LeetCode16 最接近的三数之和
+/*
+给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+返回这三个数的和。假定每组输入只存在恰好一个解。
+示例 1：
+输入：nums = [-1,2,1,-4], target = 1
+输出：2
+解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2)。
+示例 2：
+输入：nums = [0,0,0], target = 1
+输出：0
+解释：与 target 最接近的和是 0（0 + 0 + 0 = 0）。
+*/
+func TestLeetCode16(t *testing.T) {
+	fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1))
+	fmt.Println(threeSumClosest([]int{0, 0, 0}, 1))
+}
+
+// -4 -1 1 2
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	n := len(nums)
+	res := 1 << 32
+	for i := 0; i < n-2; i++ {
+		x := nums[i]
+		if i > 0 && x == nums[i-1] {
+			continue // 优化三
+		}
+
+		// 优化一
+		s := x + nums[i+1] + nums[i+2]
+		if s > target { // 后面无论怎么选，选出的三个数的和不会比 s 还小
+			if abs(s-target) < abs(res-target) {
+				res = s
+			}
+			break
+		}
+		// 优化二
+		s = x + nums[n-2] + nums[n-1]
+		if s < target { // x 加上后面任意两个数都不超过 s，所以下面的双指针就不需要跑了
+			if abs(s-target) < abs(res-target) {
+				res = s
+			}
+			continue
+		}
+		j, k := i+1, n-1
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+			if abs(sum-target) < abs(res-target) {
+				res = sum
+			}
+			if sum > target {
+				k--
+			} else if sum < target {
+				j++
+			} else {
+				return target
+			}
+		}
+	}
+	return res
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func max(a, b int) int {
 	if a > b {
 		return a
