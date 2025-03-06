c195d1b29fe7cd454692294d7b4fd0f357cc9a76
w_zhangtianyong <w_zhangtianyong@xiwang.com>
2025-03-06 17:15:48 +0800

    alg-double-point

--- algorithm/04_algorithm_double_point_slide_window_test.go
+++ algorithm/04_algorithm_double_point_slide_window_test.go
@@ -465,6 +465,47 @@ func maxProfit2(prices []int) int {
 	return res
 }
 
+// 8、LeetCode209 长度最小的子数组
+/*
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+找出该数组中满足其总和大于等于 target 的长度最小的 子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+示例 1：
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+示例 2：
+输入：target = 4, nums = [1,4,4]
+输出：1
+示例 3：
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+*/
+
+func TestLeetCode209(t *testing.T) {
+	fmt.Println(minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3}))
+	fmt.Println(minSubArrayLen(4, []int{1, 4, 4}))
+	fmt.Println(minSubArrayLen(11, []int{1, 1, 1, 1, 1, 1, 1, 1}))
+}
+
+func minSubArrayLen(target int, nums []int) int {
+	left := 0
+	n := len(nums)
+	res := n + 1
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += nums[i]
+		for sum >= target {
+			res = min(res, i-left+1)
+			sum -= nums[left]
+			left++
+		}
+	}
+	if res == n+1 {
+		res = 0
+	}
+	return res
+}
+
 func abs(x int) int {
 	if x < 0 {
 		return -x
