2172deea47132b3ea97f7a24d756ffb649a565c7
zhangtianyong26331 <zhangtianyong26331@ipalfish.com>
2025-06-25 17:01:43 +0800

    tmp

--- base/base5_test.go
+++ base/base5_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/spf13/cast"
 	"testing"
+	"time"
 )
 
 func TestNilStruct(t *testing.T) {
@@ -25,4 +26,16 @@ func TestNilStruct(t *testing.T) {
 	fmt.Println(u.Name)
 	fmt.Println(cast.ToString(0))
 
+}
+
+func TestThreeMonthAgo(t *testing.T) {
+	// 获取当前时间
+	now := time.Now()
+
+	// 计算三个月前的时间
+	threeMonthsAgo := now.AddDate(0, -3, 0)
+
+	// 打印结果
+	fmt.Println("当前时间:", now)
+	fmt.Println("三个月前的时间:", threeMonthsAgo)
 }
